2a9329cdf2c01a5864f5eec9aa0bee35dad3e05d
Luiz Rodrigues <lrodrigues@ifsp.edu.br>
2024-09-19 18:14:49 -0300

    import and use cors

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const productsRouter = require('./routes/products');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const productsRouter = require("./routes/products");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/products', productsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api/products", productsRouter);
 
 module.exports = app;
